0c0249583d64293a75791b37a226910e31c6430a
benp <benjampeterson@gmail.com>
2020-02-01 10:43:40 -0800

    art assets

---
+++ Repair Zombies/Assets/Floppimation/FloppyIK.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FloppyIK : MonoBehaviour
+{
+    public TransformTarget[] Targets;
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < Targets.Length; i++)
+        {
+            Targets[i].MatchTarget();
+        }
+    }
+}
+
+[Serializable]
+public class TransformTarget
+{
+    public Transform Visual;
+    public Transform Target;
+    public float momentumAmt;
+    public float positionRatio;
+    public float rotationRatio;
+
+    public void MatchTarget()
+    {
+        Visual.position = Vector3.Lerp(Visual.position, Target.position, positionRatio);
+        Visual.rotation = Quaternion.Lerp(Visual.rotation, Target.rotation, rotationRatio);
+        Visual.localScale = Vector3.Lerp(Visual.localScale, Target.localScale, positionRatio);
+    }
+}
