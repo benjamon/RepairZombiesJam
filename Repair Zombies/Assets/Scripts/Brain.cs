8382cc7096079a3bd30e7aab9e63e48c08856759
NICooper <nicholas.i.cooper@gmail.com>
2020-02-01 15:18:41 -0800

    LimbEffects and WIP Brain

---
+++ Repair Zombies/Assets/Scripts/Brain.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    [SerializeField]
+    private bool confused = false;
+    [SerializeField]
+    private bool right = true;
+    [SerializeField]
+    private Decision decision = Decision.idle;
+    private string target = null;
+
+    void Start() {
+        // get reference to torso component
+    }
+
+    void Update() {
+        // check if there is an obstacle, enemy, or zombie
+        if (confused) {
+            ConfusedUpdate();
+        }
+        else {
+            NormalUpdate();
+        }
+    }
+
+    private void NormalUpdate() {
+        var rayHit = getNearestRayHit();
+        // sense
+        // determine state
+        // default to moving to the right
+
+        // if a zombie is ahead of it, idle until the zombie is far enough away
+
+        // if an obstacle or enemy is ahead of it, attack
+
+        // if state has changed, update torso
+    }
+
+    private void ConfusedUpdate() {
+        // when headless, move left or right randomly for 5 seconds each, attack if anything is in front of it
+        Decision newDecision;
+        string newTarget;
+
+        var rayHit = getNearestRayHit();
+        if (rayHit.HasValue) {
+            var hitLayerName = LayerMask.LayerToName(rayHit.Value.collider.gameObject.layer);
+            switch (hitLayerName) {
+                case "Obstacle":
+                case "Zombie":
+                case "Enemy": {
+                    if (rayHit.Value.distance < 0.5f) {
+                        newDecision = Decision.attack;
+                        newTarget = hitLayerName;
+                    }
+                    break;
+                }
+            }
+        }
+        // sense
+        // determine state
+        // if state has changed, update torso
+        // if state has not changed but 5 seconds have passed, randomly change state, update torso
+    }
+
+    private RaycastHit2D? getNearestRayHit() {
+        var rayHits = Physics2D.RaycastAll(transform.position, Vector2.right, 5);
+        foreach (var rayHit in rayHits) {
+            if (rayHit.collider.gameObject != this.gameObject) {
+                return rayHit;
+            }
+        }
+        return null;
+    }
+
+    void LoseHead() {
+        confused = true;
+        ConfusedUpdate();
+    }
+
+    void GainHead() {
+        confused = false;
+        NormalUpdate();
+    }
+}
+
+public enum Decision {
+    move, attack, idle
+}
---
+++ Repair Zombies/Assets/Scripts/LimbEffectsManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LimbEffectsManager : MonoBehaviour {
+    [SerializeField]
+    private Sprite[] spriteLevels;
+    [SerializeField]
+    private float[] healthThresholds;
+    [SerializeField]
+    private ParticleSystem[] particleSystems;
+
+    private SpriteRenderer spriteRenderer;
+    private int currentLevel;
+
+    void Start() {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        currentLevel = 0;
+    }
+
+    void UpdateLimb(float health) {
+        if (health < healthThresholds[currentLevel]) {
+            currentLevel++;
+            particleSystems[currentLevel].Play();
+            spriteRenderer.sprite = spriteLevels[currentLevel];
+        }
+    }
+}
