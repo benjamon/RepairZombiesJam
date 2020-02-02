c686a43e2d707e2ed24400fe621e7c103e4036e1
benp <benjampeterson@gmail.com>
2020-02-01 21:26:06 -0800

    obstacle bounce animation

---
+++ Repair Zombies/Assets/DragObjects.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObjects : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZcoord;
+    private void OnMouseDown()
+    {
+        mZcoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mZcoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + mOffset;
+    }
+}
--- Repair Zombies/Assets/Scripts/Obstacle.cs
+++ Repair Zombies/Assets/Scripts/Obstacle.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour, IDamageable
 {
+    public float Health;
+    public SpriteRenderer Alive;
+    public SpriteRenderer Detroyed;
+    Collider2D collider;
+    Animator WobbleAnimator;
+    public float wobbleDecay = .9f;
+    float wobbleAmount = 0f;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+        WobbleAnimator = GetComponent<Animator>();
+        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Health < 0f)
+        {
+            collider.enabled = false;
+            Detroyed.enabled = true;
+            Alive.enabled = false;
+        }
+        WobbleAnimator.SetFloat("Blend", Mathf.Clamp01(wobbleAmount));
+        wobbleAmount *= wobbleDecay;
+        WobbleAnimator.speed = Mathf.Max(wobbleAmount * .5f, .7f);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DealDamage(5f);
+        }
     }
 
     public void DealDamage(float damage)
     {
+        Health -= damage;
+        wobbleAmount += damage;
     }
 }
