6172242537ccfd2293bd84c15075870488bef4f3
NICooper <nicholas.i.cooper@gmail.com>
2020-02-01 18:04:39 -0800

    merged master

---
+++ Repair Zombies/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour {
+    private Animator animator;
+    // private TorsoComponent torso;
+    void Start() {
+        // get the torso
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update() {
+        
+    }
+
+    public void Attack() {
+
+    }
+
+    public void Move(bool right) {
+
+    }
+
+    private void Stand() {
+
+    }
+
+    private void Crawl() {
+        
+    }
+}
